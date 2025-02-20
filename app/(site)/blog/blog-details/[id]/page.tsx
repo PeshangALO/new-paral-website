4e55e48be7cf8fbd11212add9363379b3a49f8bf
PeshangALO <peseng88@gmail.com>
2025-02-20 13:45:15 +0100

    7 - try

--- app/(site)/blog/blog-details/[id]/page.tsx
+++ app/(site)/blog/blog-details/[id]/page.tsx
@@ -3,14 +3,8 @@ import RelatedPost from "@/components/Blog/RelatedPost";
 import SharePost from "@/components/Blog/SharePost";
 import Image from "next/image";
 
-/**
- * Generates the list of available route params at build time.
- * This must return an array of objects (no "Promise<...>" in the type).
- */
-export async function generateStaticParams(): Promise<{ id: string }[]> {
-  return BlogData.map((post) => ({
-    id: post._id.toString(),
-  }));
+export async function generateStaticParams() {
+  return BlogData.map((post) => ({ id: post._id.toString() }));
 }
 
 export default function BlogDetailsPage({
@@ -25,33 +19,12 @@ export default function BlogDetailsPage({
   }
 
   return (
-    <section className="pb-20 pt-35 lg:pb-25 lg:pt-45 xl:pb-30 xl:pt-50">
-      <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
-        <div className="flex flex-col-reverse gap-7.5 lg:flex-row xl:gap-12.5">
-          {/* Sidebar */}
-          <div className="md:w-1/2 lg:w-[32%]">
-            <RelatedPost />
-          </div>
-
-          {/* Main Blog Content */}
-          <div className="lg:w-2/3">
-            <div className="animate_top rounded-md border border-stroke bg-white p-7.5 shadow-solid-13 dark:border-strokedark dark:bg-blacksection md:p-10">
-              <Image
-                src={blog.mainImage}
-                alt={blog.title}
-                width={700}
-                height={400}
-                className="rounded-md"
-              />
-              <h2 className="mt-6 text-3xl font-semibold text-black dark:text-white">
-                {blog.title}
-              </h2>
-              <p className="mt-4 text-gray-600">{blog.metadata}</p>
-              <SharePost />
-            </div>
-          </div>
-        </div>
-      </div>
+    <section>
+      <Image src={blog.mainImage} alt={blog.title} width={700} height={400} />
+      <h2>{blog.title}</h2>
+      <p>{blog.metadata}</p>
+      <RelatedPost />
+      <SharePost />
     </section>
   );
 }
