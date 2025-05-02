f5350d745e686bb9f3aa4cd8ecb9e77beb1048c4
PeshangALO <peseng88@gmail.com>
2025-05-02 11:19:10 +0200

    Update Calendly embed URL to include duration for scheduling

--- components/CalendlyEmbed.tsx
+++ components/CalendlyEmbed.tsx
@@ -6,7 +6,7 @@ import { InlineWidget } from "react-calendly";
 const CalendlyEmbed = () => {
   return (
     <div style={{ height: "700px" }}>
-      <InlineWidget url="https://calendly.com/peshang-paral" />
+      <InlineWidget url="https://calendly.com/peshang-paral/30min" />
     </div>
   );
 };
