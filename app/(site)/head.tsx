6d7c0445e8b49dccba95ec28199cde2fdd2e5d9a
PeshangALO <peseng88@gmail.com>
2025-08-08 13:02:12 +0200

    Integrate Google Tag Manager into head and layout components for enhanced tracking capabilities

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -6,21 +6,17 @@ export default function Head() {
       <title> Paral Dynamic | Spesialister på digitalisering, AI og andre digitale tjenester!</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description " content="Paral Dynamic utvikler løsninger spesialtilpasset din virksomhet, for å optimalisere arbeidsprosesser, øke effektiviteten og nå dine mål. Vi er her for å bygge programvaren som gjør en forskjell for deg!" />
-      {/* Google tag (gtag.js) */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17149594181"></script>
+      {/* Google Tag Manager */}
       <script dangerouslySetInnerHTML={{
         __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-17149594181');
-          gtag('event', 'conversion', {
-            'send_to': 'AW-17149594181/Ye6GCIv3kNMaEMWUyPE_',
-            'value': 1.0,
-            'currency': 'NOK'
-          });
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-P6993HGX');
         `
       }} />
+      {/* End Google Tag Manager */}
       {/* LinkedIn Insight Tag */}
       <script type="text/javascript" dangerouslySetInnerHTML={{
         __html: `
